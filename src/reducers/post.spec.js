649bd28da22343985c025624ac03d7c406fbca69
Ola Rybacka <rybacka.ola@gmail.com>
2018-09-29 01:19:16 +0200

    Devtools

--- src/reducers/post.spec.js
+++
@@ -1,8 +0,0 @@
-import reducer from "./todo"
-
-describe("Post reducer", () => {
-  test("returns a state object", () => {
-    const result = reducer(undefined, { type: "ANYTHING" })
-    expect(result).toBeDefined()
-  })
-})
